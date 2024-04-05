17a7d39e1cffde14ed3d370463903607ea5f4b4d
PushPull-Hub <hamza.dahhani@outlook.com>
2024-04-05 09:22:16 +0200

    Integrate Tailwind CSS and its dependencies

---
+++ vue-cloud/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#87CEEB",
+        "weather-secondary": "#004E71",
+      },
+    },
+  },
+  plugins: [],
+};
